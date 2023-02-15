ada1c8f4627eb0f07988b6b11eea9aab51abfa03
Ramin Aghaee <rn.aghaee@gmail.com>
2023-02-15 18:47:27 +0330

    Add project files.

---
+++ FileInfo/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace FileInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TittelBar = new System.Windows.Forms.Panel();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.lblFormName = new System.Windows.Forms.Label();
+            this.buttonX2 = new DevComponents.DotNetBar.ButtonX();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblCreate = new System.Windows.Forms.Label();
+            this.lblWrite = new System.Windows.Forms.Label();
+            this.lblAccess = new System.Windows.Forms.Label();
+            this.TittelBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TittelBar
+            // 
+            this.TittelBar.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.TittelBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(93)))), ((int)(((byte)(142)))));
+            this.TittelBar.Controls.Add(this.buttonX1);
+            this.TittelBar.Controls.Add(this.lblFormName);
+            this.TittelBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TittelBar.Location = new System.Drawing.Point(0, 0);
+            this.TittelBar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TittelBar.Name = "TittelBar";
+            this.TittelBar.Size = new System.Drawing.Size(318, 28);
+            this.TittelBar.TabIndex = 2;
+            this.TittelBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TittelBar_MouseDown);
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.BackColor = System.Drawing.Color.Transparent;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.Flat;
+            this.buttonX1.Location = new System.Drawing.Point(293, 3);
+            this.buttonX1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(22, 22);
+            this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX1.Symbol = "57676";
+            this.buttonX1.SymbolSet = DevComponents.DotNetBar.eSymbolSet.Material;
+            this.buttonX1.TabIndex = 1;
+            this.buttonX1.TextColor = System.Drawing.Color.White;
+            this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
+            // 
+            // lblFormName
+            // 
+            this.lblFormName.AutoSize = true;
+            this.lblFormName.Font = new System.Drawing.Font("Vazir", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFormName.ForeColor = System.Drawing.Color.White;
+            this.lblFormName.Location = new System.Drawing.Point(3, 4);
+            this.lblFormName.Name = "lblFormName";
+            this.lblFormName.Size = new System.Drawing.Size(53, 21);
+            this.lblFormName.TabIndex = 0;
+            this.lblFormName.Text = "FileInfo";
+            // 
+            // buttonX2
+            // 
+            this.buttonX2.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.buttonX2.ColorTable = DevComponents.DotNetBar.eButtonColor.Orange;
+            this.buttonX2.Font = new System.Drawing.Font("Vazir", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.buttonX2.Location = new System.Drawing.Point(10, 44);
+            this.buttonX2.Name = "buttonX2";
+            this.buttonX2.Size = new System.Drawing.Size(296, 32);
+            this.buttonX2.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.buttonX2.TabIndex = 1;
+            this.buttonX2.Text = "انتخاب فایل";
+            this.buttonX2.Click += new System.EventHandler(this.buttonX2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Vazir", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(228, 115);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(78, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "تاریخ ساخت :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Vazir", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(191, 172);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(115, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "تاریخ آخرین ویرایش :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Vazir", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(200, 229);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(106, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "تاریخ آخرین بازدید :";
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(9, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(300, 1);
+            this.label4.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(9, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(300, 1);
+            this.label5.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Location = new System.Drawing.Point(9, 253);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(300, 1);
+            this.label6.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Gray;
+            this.label7.Location = new System.Drawing.Point(95, 340);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(126, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Linkedin : Ramin Aghaee";
+            // 
+            // lblCreate
+            // 
+            this.lblCreate.AutoSize = true;
+            this.lblCreate.Font = new System.Drawing.Font("Vazir", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblCreate.ForeColor = System.Drawing.Color.Gray;
+            this.lblCreate.Location = new System.Drawing.Point(13, 115);
+            this.lblCreate.Name = "lblCreate";
+            this.lblCreate.Size = new System.Drawing.Size(0, 19);
+            this.lblCreate.TabIndex = 0;
+            // 
+            // lblWrite
+            // 
+            this.lblWrite.AutoSize = true;
+            this.lblWrite.Font = new System.Drawing.Font("Vazir", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblWrite.ForeColor = System.Drawing.Color.Gray;
+            this.lblWrite.Location = new System.Drawing.Point(13, 174);
+            this.lblWrite.Name = "lblWrite";
+            this.lblWrite.Size = new System.Drawing.Size(0, 19);
+            this.lblWrite.TabIndex = 0;
+            // 
+            // lblAccess
+            // 
+            this.lblAccess.AutoSize = true;
+            this.lblAccess.Font = new System.Drawing.Font("Vazir", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblAccess.ForeColor = System.Drawing.Color.Gray;
+            this.lblAccess.Location = new System.Drawing.Point(13, 231);
+            this.lblAccess.Name = "lblAccess";
+            this.lblAccess.Size = new System.Drawing.Size(0, 19);
+            this.lblAccess.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 381);
+            this.Controls.Add(this.lblAccess);
+            this.Controls.Add(this.lblWrite);
+            this.Controls.Add(this.lblCreate);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonX2);
+            this.Controls.Add(this.TittelBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FileInfo";
+            this.TittelBar.ResumeLayout(false);
+            this.TittelBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TittelBar;
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private System.Windows.Forms.Label lblFormName;
+        private DevComponents.DotNetBar.ButtonX buttonX2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblCreate;
+        private System.Windows.Forms.Label lblWrite;
+        private System.Windows.Forms.Label lblAccess;
+    }
+}
+
---
+++ FileInfo/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region DragForm & Style
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        private void TittelBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.BringToFront();
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+        #endregion
+        private void buttonX1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        string path;
+        private void buttonX2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    path = openFileDialog.FileName;
+                    DateTime Create = File.GetCreationTime(path);
+                    DateTime Write = File.GetLastWriteTime(path);
+                    DateTime Access = File.GetLastAccessTime(path);
+                    lblCreate.Text = Create.ToString();
+                    lblWrite.Text = Write.ToString();
+                    lblAccess.Text = Access.ToString();
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
